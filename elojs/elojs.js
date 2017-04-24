dfc1e1983be92346a83bb914bb9e3d99306d5043
Der3c <Der3k@protonmail.com>
2017-04-24 16:31:49 -0500

    Add exercise solutions

--- elojs/elojs.js
+++ elojs/elojs.js
@@ -5,3 +5,32 @@ for (var pyr = "#"; counter <= 7; counter++) {
   pyr += "#";
 }
 /* end exercise */
+
+/* my first shot at answering the fizzbuzz test. Return to this later if you come up with a more elegant solution. It works! */
+for (var i = 0; i <=100; i++) {
+  if (i % 3 == 0 && i % 5 == 0)
+    console.log("FizzBuzz");
+  else if (i % 3 == 0)
+    console.log("Fizz");
+  else if (i % 5 == 0)
+    console.log("Buzz");
+  else
+    console.log(i);
+  };
+/* There has to be a way to do this more effeciently, maybe ternary? */
+
+/* Printing a chess board */
+var size = 8
+var square = " "
+for (var y = 0; y <= size; y++) {
+  for (var x = 0; x <=size; x++) {
+    if ((x + y) % 2 == 0)
+      square += " ";
+    else
+      square += "#";
+  }
+  square += "\n";
+}
+console.log(square);
+/* End exercise */
+  
