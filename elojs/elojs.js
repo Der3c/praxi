91c324839346ac15e841cd06c6714301b9d52d47
Der3c <Der3k@protonmail.com>
2017-04-24 21:07:47 -0500

    add solution for project euler

--- elojs/elojs.js
+++ elojs/elojs.js
@@ -33,4 +33,13 @@ for (var y = 0; y <= size; y++) {
 }
 console.log(square);
 /* End exercise */
-  
+
+/* Project Euler - Find the sum of all multiples of 3 and 5 below 1000 */
+var total = 0
+for (var i = 1; i <= 1000; i++) {
+  if (i % 3 == 0 || i % 5 == 0)
+    total += i;
+}
+
+console.log(total);
+/* Could ommit the equal to symbol in the for statement */
