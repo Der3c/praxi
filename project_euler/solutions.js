43106f54dee2200e33ded3f0b3b70541c44f70c2
Der3c <Der3k@protonmail.com>
2017-04-25 11:24:24 -0500

    Add problem 2 solution

--- project_euler/solutions.js
+++ project_euler/solutions.js
@@ -7,3 +7,21 @@ for (var i = 1; i < 1000; i++) {
 }
 console.log(total);
 /* Problem #1 End */
+
+/* By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms. */
+/* Classic expression is Fn = Fn-1 + Fn-2. will use seed values 0 * 1 */
+function sumFibbo(limit) {
+  var sum = 0, a = 0, b = 1, f;
+
+  while (b < limit) {
+    f = a;
+    a = b;
+    b += f;
+
+    if (b % 2 == 0)
+    sum += b;
+  }
+  return sum;
+}
+console.log(sumFibbo(4000000));
+/* Problem #2 End */
