3b7f414aed0e82d70243a9c0a339ee4ee5a01ec9
Der3c <Der3k@protonmail.com>
2017-05-10 17:10:11 -0500

    Inital commit with comments for setup

--- pypraxi/pypraxi.py
+++ pypraxi/pypraxi.py
@@ -1,2 +1,6 @@
 #This would be how to make a comment in python
 print ("Hello Derek")  #Look no semicolon!
+#run from terminal by ../python filename.py
+#variables are like this:
+message = "Hello Derek"
+print(message)
