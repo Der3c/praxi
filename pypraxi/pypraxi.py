d030225d57879ddf9cc9b2f9678c1383e01db812
Der3c <Der3k@protonmail.com>
2017-05-12 16:22:56 -0500

    Add python defs

--- pypraxi/pypraxi.py
+++ pypraxi/pypraxi.py
@@ -8,3 +8,8 @@ last_name = "blaylock"
 full_name = first_name + " " + last_name
 print(full_name)
 print("Hello, " + full_name.title() + "!")
+2 + 3 ** 2
+
+#notes from here on:
+#pass method .rstrip() to remove whitespace from the string, also .lstrip(), or just .strip()
+#operators work like you would expect, use ** for exponents. 
