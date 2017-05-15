32add377edef3876d5da798ba3f1250511df739b
Der3c <Der3k@protonmail.com>
2017-05-15 08:13:58 -0500

    Add sorting list function

--- pypraxi/crashpy/cars.py
+++ pypraxi/crashpy/cars.py
@@ -1,3 +1,8 @@
 cars = ['bmw', 'audi', 'toyota', 'subaru']
 cars.sort()
 print cars
+#Can reverse the list
+cars.sort(reverse=True)
+print cars
+#Can find the length of list as follows
+print len(cars)
